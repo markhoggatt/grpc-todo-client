ac7cd931277ac756b5f58a961116fa9d707db0fb
Mark Hoggatt <mark@hoggatt.me.uk>
2023-05-28 08:01:40 +0100

    Initial Commit

---
+++ TodoClient/TodoClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoClientApp.swift
+//  TodoClient
+//
+//  Created by Mark Hoggatt on 28/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
