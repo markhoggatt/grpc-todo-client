f785cdbf8c785bcaf24139661b1b4178d9bc0a39
Mark Hoggatt <mark@hoggatt.me.uk>
2023-05-28 08:06:14 +0100

    Initial commit.

--- TodoClient/TodoClientApp.swift
+++ TodoClient/TodoClientApp.swift
@@ -8,10 +8,13 @@
 import SwiftUI
 
 @main
-struct TodoClientApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+struct TodoClientApp: App
+{
+	var body: some Scene
+	{
+		WindowGroup
+		{
+			ContentView()
+		}
+	}
 }
